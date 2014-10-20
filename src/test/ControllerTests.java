af8d1ce2321d218fa7d3571b5e3b4de4298c4687
Goldenwing <kevin.tanguay@gmail.com>
2014-10-19 21:59:36 -0400

    Update ControllerTests.java

--- src/test/ControllerTests.java
+++ src/test/ControllerTests.java
@@ -1,9 +1,29 @@
 package test;
 
-import org.junit.Test;
-import org.junit.Assert;
+import model.Connect4Stack;
+import model.PlayedToken;
 
-public class ControllerTests 
+import org.junit.*;
+
+public class ControllerTests
 {
-
+	@Test
+	public void TestStackBackendCreation()
+	{
+		Connect4Stack stackTest = new Connect4Stack(6);
+		
+		Assert.assertNotNull(stackTest);
+	}
+	
+	@Test
+	public void TestGridPaneAddToken()
+	{
+		Connect4Stack stackTest = new Connect4Stack(6);
+		PlayedToken tokenTest = new PlayedToken("red", 5, 2);
+		
+		stackTest.push(tokenTest);
+		
+		Assert.assertEquals(1, stackTest.getNumberStackElements());
+		
+	}
 }
