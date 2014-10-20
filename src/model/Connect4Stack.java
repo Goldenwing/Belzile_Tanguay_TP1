ab7dc84cbf40d6aef5f2ab81e1f1683a857783fb
Goldenwing <kevin.tanguay@gmail.com>
2014-10-19 21:53:20 -0400

    Update Connect4Stack.java

--- src/model/Connect4Stack.java
+++ src/model/Connect4Stack.java
@@ -1,55 +1,74 @@
 package model;
 import model.PlayedToken;
 
-public class Connect4Stack {
-	
+public class Connect4Stack 
+{
+	private static int nbTokens;
 	private int stackSize;
 	private final int stackCapacity;
 	private Node<PlayedToken> currentNode;
 	
-	public Connect4Stack(int capacity) {
+	public Connect4Stack(int capacity) 
+	{
 		this.stackSize = 0;
 		this.stackCapacity = capacity;
 		this.currentNode = null;
 	}
 	
-	public int getStackCapacity() {
+	public int getStackCapacity() 
+	{
 		return this.stackCapacity;
 	}
+	public static int getNbTokens()
+	{
+		return Connect4Stack.nbTokens;
+	}
 	
-	public boolean push(PlayedToken token) {
-		if (!this.isFull()) {
+	public boolean push(PlayedToken token) 
+	{
+		if (!this.isFull()) 
+		{
 			Node<PlayedToken> oldNode = this.currentNode;
 			this.currentNode = new Node<PlayedToken>(token, oldNode);
 			this.stackSize++;
+			Connect4Stack.nbTokens++;
+			
 			return true;
 		}
 		return false;
 	}
 	
-	public boolean isFull() {
+	public boolean isFull() 
+	{
 		return (this.stackSize == this.stackCapacity);
 	}
 	
-	public int getNumberStackElements() {
+	public int getNumberStackElements() 
+	{
 		return this.stackSize;
 	}
 
-	public void clear() {
+	public void clear() 
+	{
 		this.currentNode = null;
 		this.stackSize = 0;
 	}
 	
-	public PlayedToken PeekAt(int position) {
+	public PlayedToken peekAt(int position) 
+	{
 		PlayedToken peekedElement = null;
 		Node<PlayedToken> readerNode = this.currentNode;
-		for (int i = 1; i <= position; i++) {
-			if (readerNode != null) {
-				if (i == position) {
+		for (int i = 1; i <= position; i++) 
+		{
+			if (readerNode != null) 
+			{
+				if (i == position) 
+				{
 					peekedElement = readerNode.getElement();
 					break;
 				}
-				else {
+				else 
+				{
 					readerNode = readerNode.getNextNode();
 				}
 			}
@@ -60,22 +79,27 @@ public class Connect4Stack {
 		}
 		return peekedElement;
 	}
+	
 //////////////////////////////////////////////////////////////////////	
-	private class Node<T> {
+	private class Node<T> 
+	{
 		
 		private T element;
 		private Node<T> nextNode;
 		
-		public Node(T element, Node<T> next) {
+		public Node(T element, Node<T> next) 
+		{
 			this.element = element;
 			this.nextNode = next;
 		}
 		
-		public T getElement() {
+		public T getElement() 
+		{
 			return element;
 		}
 		
-		public Node<T> getNextNode() {
+		public Node<T> getNextNode() 
+		{
 			return nextNode;
 		}
 	}
